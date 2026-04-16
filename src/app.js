b9b0eafd40055c1546f90597bfa5d971f2130626
Aditya-Tiwari2005 <adityatiwarii.work@gmail.com>
2026-04-17 00:29:25 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,5 +15,12 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
  
+// routes
 
-export { app }
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter) 
+
+// https://localhost:8000//api/v1/users/register phle jaaunga /api/v1/users then from here the userRouter will activate then /register lgega toh register user call ho jaaega jab user register user ki defintion pe jaaenge (ek higher order function hai)toh ek response mil jaaega message 200 OK . 
+export { app } 
